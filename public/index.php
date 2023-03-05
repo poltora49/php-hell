b6c7723a206e5ad1b99045517ac806593cafaca3
poltora49 <poltora49@gmail.com>
2023-03-05 02:16:06 +0200

    msqli

--- public/index.php
+++ public/index.php
@@ -24,13 +24,13 @@ $db = new DATEBASE();
     <div class="container mt-5">
 
         <!-- Display any info -->
-        <?php if(isset($_REQUEST['info'])){ ?>
+        <!-- <?php if(isset($_REQUEST['info'])){ ?>
             <?php if($_REQUEST['info'] == "added"){?>
                 <div class="alert alert-success" role="alert">
                     Post has been added successfully
                 </div>
             <?php }?>
-        <?php } ?>
+        <?php } ?> -->
 
         <!-- Create a new Post button -->
         <div class="text-center">
@@ -39,17 +39,7 @@ $db = new DATEBASE();
 
         <!-- Display posts from database -->
         <div class="row">
-            <?php foreach($query as $q){ ?>
-                <div class="col-12 col-lg-4 d-flex justify-content-center">
-                    <div class="card text-white bg-dark mt-5" style="width: 18rem;">
-                        <div class="card-body">
-                            <h5 class="card-title"><?php echo $q['title'];?></h5>
-                            <p class="card-text"><?php echo substr($q['content'], 0, 50);?>...</p>
-                            <a href="view.php?id=<?php echo $q['id']?>" class="btn btn-light">Read More <span class="text-danger">&rarr;</span></a>
-                        </div>
-                    </div>
-                </div>
-            <?php }?>
+            <?php $db->listPost()?>
         </div>
        
     </div>
