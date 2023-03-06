cdf1f5961840df273272b31a1db3695313a7b0d3
poltora49 <poltora49@gmail.com>
2023-03-06 06:23:41 +0200

    del comments

--- public/logic/db.class.php
+++ public/logic/db.class.php
@@ -89,54 +89,5 @@
         }
         
     }
-    
-
-    // // Get data to display on index page
-    // $query = $connection->query( "SELECT * FROM blog_data");
-
-    // // Create a new post
-    // if(isset($_REQUEST['new_post'])){
-    //     $title = $_REQUEST['title'];
-    //     $content = $_REQUEST['content'];
-
-    //     $sql = "INSERT INTO blog_data(title, content) VALUES('$title', '$content')";
-    //     $connection->query( $sql);
-
-    //     echo $sql;
-
-    //     header("Location: index.php?info=added");
-    //     exit();
-    // }
-
-    // // Get post data based on id
-    // if(isset($_REQUEST['id'])){
-    //     $id = $_REQUEST['id'];
-    //     $sql = "SELECT * FROM blog_data WHERE id = $id";
-    //     $query = $connection->query( $sql);
-    // }
-
-    // // Delete a post
-    // if(isset($_REQUEST['delete'])){
-    //     $id = $_REQUEST['id'];
-
-    //     $sql = "DELETE FROM blog_data WHERE id = $id";
-    //     $connection->query( $sql);
-
-    //     header("Location: index.php");
-    //     exit();
-    // }
-
-    // // Update a post
-    // if(isset($_REQUEST['update'])){
-    //     $id = $_REQUEST['id'];
-    //     $title = $_REQUEST['title'];
-    //     $content = $_REQUEST['content'];
-
-    //     $sql = "UPDATE blog_data SET title = '$title', content = '$content' WHERE id = $id";
-    //     $connection->query( $sql);
-
-    //     header("Location: index.php");
-    //     exit();
-    // }
 
 ?>
